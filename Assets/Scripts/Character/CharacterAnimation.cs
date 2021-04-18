a401eb84774600f1bf2960afe49b08a632b65db8
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-18 16:08:12 +0300

    -Movement Animations added

---
+++ Assets/Scripts/Character/CharacterAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim=GetComponent<Animator>();
+    }
+    public void Run()
+    {
+        anim.SetInteger("movementMode", 2);
+    }
+    public void Walk()
+    {
+        anim.SetInteger("movementMode", 1);
+    }
+    public void Wait()
+    {
+        anim.SetInteger("movementMode", 0);
+
+    }
+}

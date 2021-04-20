e3b58f4d790749bd04ce89ad3c2bd97248538fb8
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-20 04:09:12 +0300

    -Some changes have been made to material, scripts and textures.

---
+++ Assets/Scripts/wallSection/ShowPercentage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPercentage : MonoBehaviour
+{
+    public PaintWall wall;
+    TextMeshProUGUI textmeshPro;
+    private void Start()
+    {
+         textmeshPro= GetComponent<TextMeshProUGUI>();
+
+    }
+    void Update()
+    {
+        Debug.Log(wall.percentage);
+        textmeshPro.text = "The Percentage of Red is " + wall.percentage;
+    }
+}

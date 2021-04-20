1575e17a846b9562ba30955ff9e1c03abb5d0999
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-20 04:06:16 +0300

    -PlayerInfo class created for OOP structure.

---
+++ Assets/Scripts/Character/PlayerInfo.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class PlayerInfo : MonoBehaviour
+{
+     public String name;
+     public float distance;
+     public bool finished=false;
+    public int percantageRedColor;
+    public String rank ="";
+    public PlayerInfo(String name, float distance)
+    {
+        this.name = name;
+        this.distance = distance;
+    }
+   
+    void Update()
+    {
+
+    }
+    public void isFinish()
+    {
+        finished = true;
+    }
+  
+}

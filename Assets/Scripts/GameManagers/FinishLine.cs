d032e5ecad20fb5748afb8bae35a962cc8f86e14
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-20 04:05:25 +0300

    - Commands that need to run at the endpoint have been added to the script.

---
+++ Assets/Scripts/GameManagers/FinishLine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    public GameObject paintSection;
+    public GameObject raceSection;
+
+     void Start()
+    {
+        raceSection.SetActive(true);
+        paintSection.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<CharacterManager>().Finish();
+        other.GetComponent<CharacterManager>().playerInfo.isFinish();
+        if(other.tag.Equals("mainCharacter")){
+            Camera.main.transform.rotation = Quaternion.Euler((float)-0.4, 0, 0);
+            raceSection.SetActive(false);
+            paintSection.SetActive(true);
+        }
+    }
+}

73bb38335ca41b5c3e6107b51a3a9a02137bd572
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-20 04:02:33 +0300

    -a brush has been created and mouse tracking is provided.

---
+++ Assets/Scripts/Painting/FollowMouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public Canvas myCanvas;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+        transform.position = myCanvas.transform.TransformPoint(pos);
+    }
+}

283c98f14f970cfe4592d3f835baa81e2cbf8350
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-22 02:06:23 +0300

    A script was created for the percentage bar in the Rank section.

---
+++ Assets/Scripts/GameManagers/PercentageBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[ExecuteInEditMode]
+public class PercentageBar : MonoBehaviour
+{
+    public int maximum;
+    public int current;
+    public Image mask;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetCurrentFill();
+    }
+    void GetCurrentFill()
+    {
+        //alınan değere göre barın yüzde kaç kaldığının hesaplanması
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+    }
+}

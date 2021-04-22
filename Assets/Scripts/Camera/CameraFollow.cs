149f0bf233c2aa2468e748c787e8af35e062b2a6
okcenmmustafa <mmustafaokcen@gmail.com>
2021-04-22 03:17:04 +0300

    Last changes have been made. Demo is ready.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smootSpeed = 0.150f;
+    public float smootSpeed = 0.250f;
     public Vector3 offset;
 
     void Start()
